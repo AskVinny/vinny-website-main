8fb6a525c83d156b57997829114cc3701dcdad03
RoryBuchanan <rory@askvinny.co.uk>
2024-05-21 09:53:45 +0100

    testing

--- script.js
+++ script.js
@@ -67,7 +67,7 @@ function toggleText(element) {
 document.addEventListener("DOMContentLoaded", function() {
     setTimeout(function() {
         document.getElementById('phone-popup').classList.remove('hidden');
-    }, 15000); // 30 seconds
+    }, 30000); // 30 seconds
 
     document.getElementById('submit-phone').addEventListener('click', function() {
         const phoneNumber = document.getElementById('phone-number').value;
